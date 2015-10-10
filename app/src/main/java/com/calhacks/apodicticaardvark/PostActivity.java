899f7f5b537a55a1d1d56f251229b563cbceb46f
jjluan <jjluan@berkeley.edu>
2015-10-10 14:38:34 -0700

    Fixed the POSTing of the JSON data

--- app/src/main/java/com/calhacks/apodicticaardvark/PostActivity.java
+++ app/src/main/java/com/calhacks/apodicticaardvark/PostActivity.java
@@ -7,15 +7,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.gson.Gson;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by John on 10/10/2015.
@@ -29,21 +36,38 @@ public class PostActivity extends Activity {
 
     public void post(View view){
         Poster poster = new Poster();
-        poster.execute();
+        String title = ((EditText)findViewById(R.id.titleText)).getText().toString();
+        String time = ((EditText)findViewById(R.id.timeText)).getText().toString();
+        String description = ((EditText)findViewById(R.id.descText)).getText().toString();
+        poster.execute(title,time,description);
     }
 
-    private class Poster extends AsyncTask<Void,Void,String>{
+    private class Poster extends AsyncTask<String,Void,String>{
         private static final String SERVER_URL="http://10.142.37.98:5000/receive/";
         @Override
-        protected String doInBackground(Void... params) {
+        protected String doInBackground(String... params) {
             try{
                 HttpURLConnection connection=(HttpURLConnection)new URL(SERVER_URL).openConnection();
+                connection.setDoOutput(true);
                 connection.setRequestMethod("POST");
-                OutputStream out = new BufferedOutputStream(connection.getOutputStream());
+                connection.setRequestProperty("Content-Type", "application/json");
 
-                
 
-                out.write(new Gson().toJson("test").getBytes());
+                Map<String,String>info = new HashMap<String,String>();
+                info.put("Title",params[0]);
+                info.put("Time",params[1]);
+                info.put("Description",params[2]);
+                Writer out = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(),"UTF-8"));
+                String json = new Gson().toJson(info, info.getClass());
+                Log.d("DEBUG", "JSON content: "+json);
+                Log.d("DEBUG", "Writing to stream: " + Arrays.toString(json.getBytes()));
+                out.write(json);
+                out.flush();
+                out.close();
+                int resp = connection.getResponseCode();
+                if(resp!=200){
+                    Log.d("DEBUG","Network error. Status: "+resp);
+                }
                 connection.disconnect();
             } catch(Exception e){
                 e.printStackTrace();
