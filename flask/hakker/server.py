a08c40ccdb0d0524845c6ca8e46122b0fc1aec82
Howard <lindennew@gmail.com>
2015-10-11 02:37:59 -0700

    user data stored locally, complete join

--- flask/hakker/server.py
+++ flask/hakker/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, g, session, redirect, escape, flash, jsonify, Response
+from flask import Flask, url_for, request, session, redirect, escape, flash, jsonify, Response
 from werkzeug import check_password_hash, generate_password_hash
 from date_convert import *
 import json
@@ -13,23 +13,31 @@ def index():
 		return 'Logged in as %s' % escape(session['username'])
 	return 'You are not logged in'
 
-@app.route('/register/', methods = ['POST'])
+@app.route('/register', methods = ['POST'])
 def register():
 	if 'username' in session:
 		return 'already registered'
 	if request.method == 'POST':
 		userin = request.get_json(force = True)
-		db[userin['username']] = {'id': str(len(db.keys())),'username':userin['username'], 'pw_hash': generate_password_hash(userin['password'])}
+		with open("data.json", "r") as fp:
+			db = json.load(fp)
+		if userin['username'] in db:
+			return 'username already exist. choose another one.'
+		db[userin['username']] = {'username':userin['username'], 'pw_hash': generate_password_hash(userin['password']), 'rating': 3, 'currentEvents': [], 'pastEvents': []}
+		with open("data.json", "w") as fp:
+			json.dump(db, fp)
 		print('You are now registered.')
 		print(db)
 		return 'registered'
 
 
-@app.route('/login/', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
 	if 'username' in session:
 		return 'already signed in'
 	userin = request.get_json(force = True)	
+	with open("data.json", "r") as fp:
+			db = json.load(fp)
 	if userin['username'] in db:
 		if not check_password_hash(db[userin['username']]['pw_hash'], userin['password']):
 			return 'wrong password'
@@ -39,13 +47,16 @@ def login():
 	else:
 		return 'no such username'
 
-@app.route('/logout/')
+@app.route('/logout')
 def logout():
     # remove the username from the session if it's there
-    print(session)
-    session.pop('username', None)
-    print(session)
-    return 'you are logged out'
+    if 'username' in session:
+    	print(session)
+    	session.pop('username', None)
+    	print(session)
+    	return 'you are logged out'
+    return 'you are not logged in'
+
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
@@ -58,23 +69,41 @@ def board():
 
 @app.route('/receive/', methods = ['POST'])
 def post():
+	#with open("message_data.json", "r") as fp:
+	#	message_db = json.load(fp)
 	new = request.get_json(force = True)
+	new['Participants_list'] = []
 	message_db.append(new)
+	with open("message_data.json", "w") as fp:
+			json.dump(message_db, fp)
 	print(new)
 	print(message_db)
 	return 'ok'
 
 @app.route('/join', methods = ['POST'])
 def join():
-	request_title = request.get_json(force = True)
+	request_join = request.get_json(force = True)
+	with open("message_data.json", "r") as fp:
+		message_db = json.load(fp)
 	for post in message_db:
-		if not post['Title'] == request_title['Title']:
+		if not post['Title'] == request_join['Title']:
 			return 'the post you are looking for does not exist'
 		elif expire(post['Time']):
 			message_db.remove(post)
+			with open("message_data.json", "w") as fp:
+				json.dump(message_db, fp)
 			return 'the post you are looking for has expired'
 		else:
-			pass # add in the user
+			if len(post['Participants_list']) < post['Participants']:
+				post['Participants_list'].append(request_join['Name'])
+				db[session['username']]['currentEvents'].append(post)
+				print('successfully joined')
+				if len(post['Participants_list'] == post['Participants']):
+					message_db.remove(post)
+					with open("message_data.json", "w") as fp:
+						json.dump(message_db, fp)
+					return 'post complete'
+				return 'successfully joined'
 	return 'no post right now'
 
 if __name__ == '__main__':
